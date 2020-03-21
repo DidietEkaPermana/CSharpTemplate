f6928a4120e2c40039fd41a175b9aa205c05f7b8
didietekapermana <didiet_eka_permana@hotmail.com>
2020-03-21 11:39:40 +0700

    add openapi

--- Service.WebApi/Startup.cs
+++ Service.WebApi/Startup.cs
@@ -9,6 +9,7 @@ using Serilog.Exceptions;
 using Serilog.Sinks.Elasticsearch;
 using Service.Core;
 using System;
+using Microsoft.OpenApi.Models;
 
 namespace Service.WebApi
 {
@@ -45,6 +46,28 @@ namespace Service.WebApi
             services.AddService(typeof(Startup));
 
             //services.AddMessagingService();
+
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo {
+                    Title = "My API",
+                    Version = "v1",
+                    Description = "A simple example ASP.NET Core Web API",
+                    TermsOfService = new Uri("https://example.com/terms"),
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Didiet Eka Permana",
+                        Email = "didiet.permana@gmail.com",
+                        Url = new Uri("https://github.com/didietekapermana"),
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "Use under MIT",
+                        Url = new Uri("https://example.com/license"),
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,6 +77,14 @@ namespace Service.WebApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.AddMiddleware();
 
